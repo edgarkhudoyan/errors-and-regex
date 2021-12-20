f2fa5ba5f49224f864934176748cf72f1f42a832
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-20 13:48:14 +0400

    feat: add task3.js

---
+++ 3-convert.js
@@ -0,0 +1,9 @@
+/* Convert the code using try...catch.
+
+function reverseString(s) {
+typeof s !== "string"
+? console.log("s.split is not a function")
+: (s = s.split("").reverse().join(""));
+console.log(s);
+} */
+'use strict';
