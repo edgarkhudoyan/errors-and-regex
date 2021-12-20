f932ab5d40c02aef6fbe6826ba32729b0ae1a7a7
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-20 13:40:10 +0400

    feat: add isPositive function

--- 2-isPositive.js
+++ 2-isPositive.js
@@ -3,3 +3,20 @@ return the string YES. Otherwise, it must throw an Error according to the follow
 . If a is 0, throw an Error with message = Zero Error.
 . If a is negative, throw an Error with message = Negative Error. */
 'use strict';
+function isPositive(a) {
+  try {
+    if (a > 0) {
+      console.log('YES');
+    } else if (a === 0) {
+      throw new Error('Zero Error');
+    } else if (a < 0) {
+      throw new Error('Negative Error');
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+isPositive(12); // Prints YES
+isPositive(0); // throws Error: Zero Error
+isPositive(-5); // throws Error: Negative Error
