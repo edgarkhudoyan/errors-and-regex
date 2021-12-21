a23c0b2fbadbb729fc6760700ebfa922b7c27afc
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-22 00:19:48 +0400

    feat: add myRegExp.js

---
+++ myRegExp.js
@@ -0,0 +1,5 @@
+/* Create a RegExp myRegExp to test if a string is a valid pin or not. A valid pin has:
+Exactly 4 or 6 characters.
+Only numerical characters (0-9).
+No whitespace. */
+'use strict';
